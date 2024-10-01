6a6007f0a04916ac94fcd385a15f35ceda3edb39
Jatin Maurya <mauryajatin45@gmail.com>
2024-10-01 15:37:57 +0530

    Initial commit

---
+++ JS/temp/app.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("idno1"));
+
+console.log(document.getElementsByTagName("p")) ;
+
+document.getElementById("idno1").innerText = "Hi, I changed something to try!!!" 
