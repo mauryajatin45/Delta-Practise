26049ba3ce69c6acdbc8ba8c95cd4990cc111658
Jatin Maurya <mauryajatin45@gmail.com>
2024-11-05 19:26:54 +0530

    practised backed

--- Backend/temp/index.js
+++ Backend/temp/index.js
@@ -1,2 +1,37 @@
-const express = require('express');
+const express = require ("express");
 const app = express();
+
+let port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`Port running on localhost:${port}`);
+})
+
+// app.use((req, res)=>{
+//     console.log("request received");
+//     res.send({name: 'Jatin', 
+//         age:18,
+//         Maratial_status: "Single"
+//     });
+// })
+
+// app.get('/', (req, res)=>{
+//     res.send('You are on the root path');
+// });
+
+// app.get ('/search', (req, res)=>{
+//     res.send('Searching for you!!!');
+// });
+
+// app.get('*', (req, res)=>{
+//     res.send('Ye page exist nhai karta ha, Dhanayavad!!!');
+// });
+
+// app.post('/', (req, res)=>{
+//     res.send('You sent a post request');
+// });
+
+app.get('/:username', (req, res)=>{
+    let {username} = req.params;
+    res.send(`You are viewing the page of ${username}`);
+});
