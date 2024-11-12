431d5ea6852674e7205cc951507f46684b3c0dc4
Jatin Maurya <mauryajatin45@gmail.com>
2024-11-12 11:27:48 +0530

    Ejs Completed

--- Backend/EJS/app.js
+++ Backend/EJS/app.js
@@ -3,9 +3,28 @@ const app = express();
 
 const port =3000; 
 
-app.get('/', (req, res) =>{
-    // res.send("Response received successfully");
-    res.render("home.ejs")
+// app.get('/', (req, res) =>{
+//     // res.send("Response received successfully");
+//     res.render("home.ejs")
+// });
+
+// app.get('/rolldice', (req, res) =>{
+//     let value = Math.ceil(Math.random()*6);
+//     res.render("rolldice.ejs", {
+//         num: value
+//     })
+// });
+
+app.get('/ig/:username', (req, res)=>{
+    const instadata = require("./data.json");
+    const {username} = req.params;
+    const data = instadata[username];
+    if(data){
+        res.render("instagram.ejs", {data});
+        console.log(data);
+    }else{
+        res.render("error.ejs", {username});
+    }
 });
 
 app.listen(port, ()=>{
