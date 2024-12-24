5a6281f72063aee786abbc54f61e729039659c49
Jatin Maurya <mauryajatin45@gmail.com>
2024-12-24 08:58:46 +0530

    Working on the React Day-2

--- React/first_project/src/App.jsx
+++ React/first_project/src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Title from './Title'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Title></Title>
+    <h1>I am from App Component</h1>
+    {/* <div className="first">
+      <h1>Hello World!</h1>
+      <p>This is a paragraph tag </p>
+      <button>ma button hu</button>
+    </div>
+
+    <div className="second">
+      <h1>ma dusara div hu bhai</h1>
+      <button>aur ma dusara button</button>
+    </div> */}
     </>
   )
 }
---
+++ React/first_project/src/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+  return <h1>This is the Title Component</h1>;
+}
+
+ export default Title;
